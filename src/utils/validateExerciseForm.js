d3cb34845f6b97085bc87872a04430dd56a947c1
Shannon Bentley <bentley.shannon@yahoo.com>
2022-11-08 21:26:20 -0600

    added required for fullname, email, and password

--- src/utils/validateExerciseForm.js
+++ src/utils/validateExerciseForm.js
@@ -1,5 +1,20 @@
 export const validateExerciseForm = (values) => {
     const errors = {};
 
-    if ()
+    if (!values.fullName) {
+        errors.fullName = 'Required';
+    } 
+
+    if (!values.email) {
+        errors.email = 'Required';
+    } else if (!values.email.includes('@')) {
+        errors.email = 'Email should contain an @.';
+    }
+
+    if (!values.password) {
+        errors.password = 'Required';
+    }
+
+    return errors;
+    
 }
