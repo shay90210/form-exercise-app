85f1b50ee25a036302543fa32e74887374d1c638
Shannon Bentley <bentley.shannon@yahoo.com>
2022-11-08 22:02:50 -0600

    added classNames for styling purposes

--- src/components/FormExercise.js
+++ src/components/FormExercise.js
@@ -5,7 +5,10 @@ import {
     FormGroup,
     Col,
     Input,
-    Button
+    Button,
+    CardBody,
+    Card,
+    Container
 } from 'reactstrap';
 import { validateExerciseForm } from '../utils/validateExerciseForm';
 
@@ -13,7 +16,7 @@ const FormExercise = () => {
 
     return (
         <div>
-            <h1>Shannon Bentley's Form Exercise</h1>
+            <h1 className='form-title'>Shannon Bentley's Form Exercise</h1>
                 <Formik
                     initialValues={{
                         fullName: '',
@@ -24,124 +27,132 @@ const FormExercise = () => {
                     }}
                     validate={validateExerciseForm}
                 >
-                    <Form className='form-styles'>
-                        <FormGroup>
-                            <Label htmlFor='fullName'>Full Name</Label>
-                            <Col>
-                                <Field 
-                                    name='fullName'
-                                    placeholder='Full Name'
-                                    className='form-control'
-                                />
-                                <ErrorMessage name='fullName'>
-                                    {(msg) => <p className='text-danger'>{msg}</p>}
-                                </ErrorMessage>
-                            </Col>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label htmlFor='email'>Email</Label>
-                            <Col>
-                                <Field 
-                                    name='email'
-                                    placeholder='Email'
-                                    className='form-control'
-                                />
-                                <ErrorMessage name='email'>
-                                    {(msg) => <p className='text-danger'>{msg}</p>}
-                                </ErrorMessage>
-                            </Col>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label htmlFor='password'>Password</Label>
-                                <Field 
-                                    name='password'
-                                    placeholder='Password'
-                                    className='form-control'
-                                />
-                                <ErrorMessage name='password'>
-                                    {(msg) => <p className='text-danger'>{msg}</p>}
-                                </ErrorMessage>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label htmlFor='occupation'>Occupation</Label>
-                                <Input 
-                                    id='occupation'
-                                    name='Select Occupation'
-                                    type='select'
-                                >
-                                    <option>occupation1</option>
-                                    <option>occupation2</option>
-                                    <option>occupation3</option>
-                                    <option>occupation4</option>
-                                    <option>occupation5</option>
-                                    <option>occupation6</option>
-                                    <option>occupation7</option>
-                                    <option>occupation8</option>
-                                </Input>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label htmlFor='state'>State</Label>
-                                <Input
-                                    id='state'
-                                    name='Select State'
-                                    type='select'
-                                >
-                                    <option value="AL">Alabama</option>
-                                    <option value="AK">Alaska</option>
-                                    <option value="AZ">Arizona</option>
-                                    <option value="AR">Arkansas</option>
-                                    <option value="CA">California</option>
-                                    <option value="CO">Colorado</option>
-                                    <option value="CT">Connecticut</option>
-                                    <option value="DE">Delaware</option>
-                                    <option value="DC">District Of Columbia</option>
-                                    <option value="FL">Florida</option>
-                                    <option value="GA">Georgia</option>
-                                    <option value="HI">Hawaii</option>
-                                    <option value="ID">Idaho</option>
-                                    <option value="IL">Illinois</option>
-                                    <option value="IN">Indiana</option>
-                                    <option value="IA">Iowa</option>
-                                    <option value="KS">Kansas</option>
-                                    <option value="KY">Kentucky</option>
-                                    <option value="LA">Louisiana</option>
-                                    <option value="ME">Maine</option>
-                                    <option value="MD">Maryland</option>
-                                    <option value="MA">Massachusetts</option>
-                                    <option value="MI">Michigan</option>
-                                    <option value="MN">Minnesota</option>
-                                    <option value="MS">Mississippi</option>
-                                    <option value="MO">Missouri</option>
-                                    <option value="MT">Montana</option>
-                                    <option value="NE">Nebraska</option>
-                                    <option value="NV">Nevada</option>
-                                    <option value="NH">New Hampshire</option>
-                                    <option value="NJ">New Jersey</option>
-                                    <option value="NM">New Mexico</option>
-                                    <option value="NY">New York</option>
-                                    <option value="NC">North Carolina</option>
-                                    <option value="ND">North Dakota</option>
-                                    <option value="OH">Ohio</option>
-                                    <option value="OK">Oklahoma</option>
-                                    <option value="OR">Oregon</option>
-                                    <option value="PA">Pennsylvania</option>
-                                    <option value="RI">Rhode Island</option>
-                                    <option value="SC">South Carolina</option>
-                                    <option value="SD">South Dakota</option>
-                                    <option value="TN">Tennessee</option>
-                                    <option value="TX">Texas</option>
-                                    <option value="UT">Utah</option>
-                                    <option value="VT">Vermont</option>
-                                    <option value="VA">Virginia</option>
-                                    <option value="WA">Washington</option>
-                                    <option value="WV">West Virginia</option>
-                                    <option value="WI">Wisconsin</option>
-                                    <option value="WY">Wyoming</option>
-                                </Input>
-                        </FormGroup>
-                
-                        <Button>Submit</Button>
-                    </Form>
+                    <Container className='mt-5'>
+                        <Card>
+                            <CardBody>
+                                <Form className='form-style'>
+                                    <FormGroup>
+                                        <Label htmlFor='fullName' className='label-style'>Full Name</Label>
+                                        <Col>
+                                            <Field 
+                                                name='fullName'
+                                                placeholder='Full Name'
+                                                className='form-control'
+                                            />
+                                            <ErrorMessage name='fullName'>
+                                                {(msg) => <p className='text-danger'>{msg}</p>}
+                                            </ErrorMessage>
+                                        </Col>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label htmlFor='email' className='label-style'>Email</Label>
+                                        <Col>
+                                            <Field 
+                                                name='email'
+                                                placeholder='Email'
+                                                className='form-control'
+                                            />
+                                            <ErrorMessage name='email'>
+                                                {(msg) => <p className='text-danger'>{msg}</p>}
+                                            </ErrorMessage>
+                                        </Col>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label htmlFor='password' className='label-style'>Password</Label>
+                                            <Field 
+                                                name='password'
+                                                placeholder='Password'
+                                                className='form-control'
+                                            />
+                                            <ErrorMessage name='password'>
+                                                {(msg) => <p className='text-danger'>{msg}</p>}
+                                            </ErrorMessage>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label htmlFor='occupation' className='label-style'>Occupation</Label>
+                                            <Input 
+                                                id='occupation'
+                                                name='Select Occupation'
+                                                type='select'
+                                            >
+                                                <option>occupation1</option>
+                                                <option>occupation2</option>
+                                                <option>occupation3</option>
+                                                <option>occupation4</option>
+                                                <option>occupation5</option>
+                                                <option>occupation6</option>
+                                                <option>occupation7</option>
+                                                <option>occupation8</option>
+                                            </Input>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label htmlFor='state' className='label-style'>State</Label>
+                                            <Input
+                                                id='state'
+                                                name='Select State'
+                                                type='select'
+                                            >
+                                                <option value="AL">Alabama</option>
+                                                <option value="AK">Alaska</option>
+                                                <option value="AZ">Arizona</option>
+                                                <option value="AR">Arkansas</option>
+                                                <option value="CA">California</option>
+                                                <option value="CO">Colorado</option>
+                                                <option value="CT">Connecticut</option>
+                                                <option value="DE">Delaware</option>
+                                                <option value="DC">District Of Columbia</option>
+                                                <option value="FL">Florida</option>
+                                                <option value="GA">Georgia</option>
+                                                <option value="HI">Hawaii</option>
+                                                <option value="ID">Idaho</option>
+                                                <option value="IL">Illinois</option>
+                                                <option value="IN">Indiana</option>
+                                                <option value="IA">Iowa</option>
+                                                <option value="KS">Kansas</option>
+                                                <option value="KY">Kentucky</option>
+                                                <option value="LA">Louisiana</option>
+                                                <option value="ME">Maine</option>
+                                                <option value="MD">Maryland</option>
+                                                <option value="MA">Massachusetts</option>
+                                                <option value="MI">Michigan</option>
+                                                <option value="MN">Minnesota</option>
+                                                <option value="MS">Mississippi</option>
+                                                <option value="MO">Missouri</option>
+                                                <option value="MT">Montana</option>
+                                                <option value="NE">Nebraska</option>
+                                                <option value="NV">Nevada</option>
+                                                <option value="NH">New Hampshire</option>
+                                                <option value="NJ">New Jersey</option>
+                                                <option value="NM">New Mexico</option>
+                                                <option value="NY">New York</option>
+                                                <option value="NC">North Carolina</option>
+                                                <option value="ND">North Dakota</option>
+                                                <option value="OH">Ohio</option>
+                                                <option value="OK">Oklahoma</option>
+                                                <option value="OR">Oregon</option>
+                                                <option value="PA">Pennsylvania</option>
+                                                <option value="RI">Rhode Island</option>
+                                                <option value="SC">South Carolina</option>
+                                                <option value="SD">South Dakota</option>
+                                                <option value="TN">Tennessee</option>
+                                                <option value="TX">Texas</option>
+                                                <option value="UT">Utah</option>
+                                                <option value="VT">Vermont</option>
+                                                <option value="VA">Virginia</option>
+                                                <option value="WA">Washington</option>
+                                                <option value="WV">West Virginia</option>
+                                                <option value="WI">Wisconsin</option>
+                                                <option value="WY">Wyoming</option>
+                                            </Input>
+                                    </FormGroup>
+                            
+                                    <Button
+                                        color= 'primary'
+                                    >Submit</Button>
+                                </Form>
+                            </CardBody>
+                        </Card>
+                    </Container>
                 </Formik>
         </div>
     )
